dcee3a26ef69eeba194c2722e772acf6fec4f84e
Dhanuvnayak <dhanalakshmi.v@centelon.com>
2023-01-09 16:14:12 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ var adminRoutes = require('./routes/adminRoute')
 
 
 // process.env.PORT hoy to e else 3000
-const PORT = process.env.PORT || 10000
+const PORT = process.env.PORT || 3000
 
 // some useful library
 app.use(bodyParser.urlencoded({ extended: true }))
